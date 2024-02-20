75e27786b6153117a176c6316341e1ff1c99a89c
kimjereen <kimbarroquillo@gmail.com>
2024-02-20 14:52:27 +0800

    with coming soon and is integrated and edited

--- header.php
+++ header.php
@@ -20,56 +20,56 @@
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">About</a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Vision & Mission</a></li>
-            <li><a class="dropdown-item" href="#">History</a></li>
-            <li><a class="dropdown-item" href="#">Objectives</a></li>
-            <li><a class="dropdown-item" href="#">Organization</a></li>
-            <li><a class="dropdown-item" href="#">Administration</a></li>
-            <li><a class="dropdown-item" href="#">Departments</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">Vision & Mission</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">History</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">Objectives</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">Organization</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">Administration</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">Departments</a></li>
           </ul>
         </li>
 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Academic Programs</a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Doctor of Veterinary Medicine</a></li>
-            <li><a class="dropdown-item" href="#">Master of Veterinary Medicine</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">Doctor of Veterinary Medicine</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">Master of Veterinary Medicine</a></li>
           </ul>
         </li>
 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Research</a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">On-going Project</a></li>
-            <li><a class="dropdown-item" href="#">Publication</a></li>
-            <li><a class="dropdown-item" href="#">Research Partners</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">On-going Project</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">Publication</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">Research Partners</a></li>
           </ul>
         </li>
 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Updates</a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">News</a></li>
-            <li><a class="dropdown-item" href="#">Events & Announcements</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">News</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">Events & Announcements</a></li>
           </ul>
         </li>
 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Resource & Facilities</a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Lecture Halls</a></li>
-            <li><a class="dropdown-item" href="#">Laboratories/a></li>
-            <li><a class="dropdown-item" href="#">CenTrAD</a></li>
-            <li><a class="dropdown-item" href="#">e-Histology</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">Lecture Halls</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">Laboratories/a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">CenTrAD</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">e-Histology</a></li>
           </ul>
         </li>
 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Extension</a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Vet Mission</a></li>
-            <li><a class="dropdown-item" href="#">Cattle Dispersal</a></li>
-            <li><a class="dropdown-item" href="#">Veterinary Teaching Hospital</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">Vet Mission</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">Cattle Dispersal</a></li>
+            <li><a class="dropdown-item" href="assets/coming-soon/index.html">Veterinary Teaching Hospital</a></li>
           </ul>
         </li>
 
