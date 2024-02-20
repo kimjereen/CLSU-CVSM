73af91679a61438a1120cb2e0cd8697646cfdbf0
sweetreverie442 <146345346+sweetreverie442@users.noreply.github.com>
2024-02-20 21:52:24 +0800

    Update index.php
    updated imaages on research and facilities

--- index.php
+++ index.php
@@ -282,7 +282,7 @@
         <!-- About Us Learn More -->
         <div class="col-md-6 position-relative">
           <div class="ov-image-container">
-            <img src="assets/images/home/facade.jpg" class="img-fluid" alt="Picture 2">
+            <img src="assets/images/Student - Dog checking.jpeg" class="img-fluid" alt="Picture 2">
             <div class="ov-overlay"></div>
             <div class="ov-content">
               <h3 class="ov-title">About Us</h3>
@@ -299,7 +299,7 @@
         <!-- Research & Publications Learn More -->
         <div class="col-md-6 position-relative">
           <div class="ov-image-container">
-            <img src="assets/images/home/facade.jpg" class="img-fluid" alt="Picture 2">
+            <img src="assets/images/temporary/STudent - chicken sorting.jpg" class="img-fluid" alt="Picture 2">
             <div class="ov-overlay"></div>
             <div class="ov-content">
               <h3 class="ov-title">Research & Publications</h3>
@@ -311,7 +311,7 @@
 
         <!-- Research & Publications Picture-->
         <div class="col-md-6">
-          <img src="assets/images/home/facade.jpg" class="img-fluid" alt="Picture 4">
+          <img src="assets/images/temporary/Student - Catheter.jpeg" class="img-fluid" alt="Picture 4">
         </div>
       </div>
     </div>
